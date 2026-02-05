92f9f36270b8da06a377ada1702a9ecca46a6e0d
DarioDang <ankhuong9999@gmail.com>
2026-02-06 12:38:28 +1300

    Update dim_zones sql script into git.

--- 04-analytics-engineering/ny_taxi_dbt/models/marts/dim_zones.sql
+++ 04-analytics-engineering/ny_taxi_dbt/models/marts/dim_zones.sql
@@ -1,7 +1,16 @@
 with taxi_zone_lookup as (
     SELECT *  
     FROM {{ ref('taxi_zone_lookup') }}
+),
+
+renamed AS (
+    SELECT
+        locationID as location_id,
+        borough,
+        zone,
+        service_zone
+    FROM taxi_zone_lookup
 )
 
 SELECT * 
-FROM taxi_zone_lookup
+FROM renamed
