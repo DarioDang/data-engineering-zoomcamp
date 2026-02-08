d5efdf83370b0f15d1741a8bc29d860f313d5503
Dario Dang <dariodang9999@gmail.com>
2026-02-08 04:20:53 +0000

    adjust macros function get_vendor_name()

--- 04-analytics-engineering/ny_taxi_dbt/macros/get_vendor_names.sql
+++ 04-analytics-engineering/ny_taxi_dbt/macros/get_vendor_names.sql
@@ -1,7 +1,16 @@
-{% macro get_vendor_names(vendor_id) -%}
-case
-  when cast({{ vendor_id }} as int64) = 1 then 'Creative Mobile Technologies, LLC'
-  when cast({{ vendor_id }} as int64) = 2 then 'VeriFone Inc.'
-  else 'Unknown'
+{% macro get_vendor_data(vendor_id_column) %}
+
+{% set vendors = {
+    1: 'Creative Mobile Technologies',
+    2: 'VeriFone Inc.',
+    4: 'Unknown/Other'
+} %}
+
+case {{ vendor_id_column }}
+    {% for vendor_id, vendor_name in vendors.items() %}
+    when {{ vendor_id }} then '{{ vendor_name }}'
+    {% endfor %}
 end
-{%- endmacro %}
+
+{% endmacro %}
+
